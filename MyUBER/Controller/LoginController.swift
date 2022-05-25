9e20f01fbc5de307b38017a55be9adcf734d7a7b
gR <Gr>
2022-05-25 07:43:52 +0300

    Initial Commit

---
+++ MyUBER/Controller/LoginController.swift
@@ -0,0 +1,108 @@
+//
+//  LoginController.swift
+//  MyUBER
+//
+//  Created by admin on 23.05.2022.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    //MARK: - Properties
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+
+    private lazy var emailContainerView: UIView = {
+        let view =  UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"), textField: emailTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+
+    private let emailTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Email", isSecureTextEntry: false)
+    }()
+
+    private let passwordTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Password", isSecureTextEntry: true)
+    }()
+
+    private let loginButton: AuthButton = {
+        let button = AuthButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ",
+                                                        attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+                                                                     .foregroundColor : UIColor.lightGray])
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [.font : UIFont.boldSystemFont(ofSize: 16),
+                                                                                  .foregroundColor : UIColor.mainBlueTint]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.addTarget(self, action: #selector(handleShowSignUP), for: .touchUpInside)
+
+        return button
+    }()
+
+    //MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    //MARK: - SELECTORS
+
+    @objc func handleShowSignUP() {
+        let controller = SignUpController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
+
+    //MARK: - Helepers
+
+    func configureUI() {
+        configureNavigationBar()
+
+        view.backgroundColor = .backgroundColor
+
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
+
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView,
+                                                   loginButton])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 24
+
+        view.addSubview(stack)
+        stack.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right:  view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16)
+
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, height: 32)
+    }
+
+    func configureNavigationBar() {
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+    }
+}
+
