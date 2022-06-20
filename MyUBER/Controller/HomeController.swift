d313c0d52a94f5efba23b0e544cc68997fbe0fa0
gR <Gr>
2022-06-20 20:24:46 +0300

    update position

--- MyUBER/Controller/HomeController.swift
+++ MyUBER/Controller/HomeController.swift
@@ -42,13 +42,29 @@ class HomeController: UIViewController {
     }
 
     //MARK: - API
+    
     func fetchDrivers() {
         guard let location = locationManager?.location else { return }
+
         Service.shared.fetchDrivers(location: location) { driver in
             guard let coordinate = driver.location?.coordinate else { return }
             let annotation = DriverAnnotation(uid: driver.uid, coordinate: coordinate)
-            
-            self.mapView.addAnnotation(annotation)
+
+
+            var driverIsVisible: Bool {
+                return self.mapView.annotations.contains { annotation in
+                    guard let driverAnno = annotation as? DriverAnnotation else { return false}
+                    if driverAnno.uid == driver.uid {
+                        driverAnno.updateAnnotationPosition(withCoordinate: coordinate)
+                        return true
+                    }
+                    return false
+                }
+            }
+
+            if !driverIsVisible {
+                self.mapView.addAnnotation(annotation)
+            }
         }
     }
 
--- MyUBER/Model/DriverAnnotation.swift
+++ MyUBER/Model/DriverAnnotation.swift
@@ -9,7 +9,7 @@ import MapKit
 
 class DriverAnnotation: NSObject, MKAnnotation {
 
-    var coordinate: CLLocationCoordinate2D
+    dynamic var coordinate: CLLocationCoordinate2D
     var uid: String
 
     init(uid: String, coordinate: CLLocationCoordinate2D) {
@@ -17,4 +17,9 @@ class DriverAnnotation: NSObject, MKAnnotation {
         self.coordinate = coordinate
     }
 
+    func updateAnnotationPosition(withCoordinate coordinate: CLLocationCoordinate2D) {
+        UIView.animate(withDuration: 0.2) {
+            self.coordinate = coordinate
+        }
+    }
 }
