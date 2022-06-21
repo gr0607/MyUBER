0b132713392f508f3b74e00f32e49382e539616e
gR <Gr>
2022-06-21 21:07:22 +0300

    displaying search location on map

--- MyUBER/Controller/HomeController.swift
+++ MyUBER/Controller/HomeController.swift
@@ -164,6 +164,17 @@ class HomeController: UIViewController {
 
         view.addSubview(tableView)
     }
+
+    func dismissLocationView(completion: ((Bool) -> Void)? = nil) {
+        UIView.animate(withDuration: 0.3, animations:  {
+            self.locationInputView.alpha = 0
+            self.tableView.frame.origin.y = self.view.frame.height
+            self.locationInputView.removeFromSuperview()
+            UIView.animate(withDuration: 0.5,animations: {
+                self.inputActivationView.alpha = 1
+            })}, completion: completion)
+        }
+
 }
 
 //MARK: - Map Helper Functions
@@ -248,16 +259,7 @@ extension HomeController: LocationInputViewDelegate {
     }
 
     func dismissLocationInputView() {
-
-        UIView.animate(withDuration: 0.3) {
-            self.locationInputView.alpha = 0
-            self.tableView.frame.origin.y = self.view.frame.height
-        } completion: { _ in
-            self.locationInputView.removeFromSuperview()
-            UIView.animate(withDuration: 0.3) {
-                self.inputActivationView.alpha = 1
-            }
-        }
+       dismissLocationView()
     }
 }
 
@@ -284,5 +286,15 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
         }
 
         return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedPlacemark = searchResults[indexPath.row]
+        dismissLocationView { _ in
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = selectedPlacemark.coordinate
+            self.mapView.addAnnotation(annotation)
+            self.mapView.selectAnnotation(annotation, animated: true)
+        }
     }
 }
