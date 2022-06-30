3ab13e2650720f056d1b85bf6f36d313ba2fa2a2
gR <Gr>
2022-06-30 07:43:53 +0300

    add custom init with user

--- MyUBER/View/MenuHeader.swift
+++ MyUBER/View/MenuHeader.swift
@@ -10,12 +10,14 @@ import UIKit
 class MenuHeader: UIView {
 
     //MARK: - Properties
-    var user: User? {
-        didSet {
-            fullnameLabel.text = user?.fullname
-            emailLabel.text = user?.email
-        }
-    }
+//    var user: User? {
+//        didSet {
+//            fullnameLabel.text = user?.fullname
+//            emailLabel.text = user?.email
+//        }
+//    }
+
+    private let user: User
 
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
@@ -23,27 +25,29 @@ class MenuHeader: UIView {
         return iv
     }()
 
-    private let fullnameLabel: UILabel = {
+    private lazy var fullnameLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 16)
         label.textColor = .white
-        label.text = "Some Name"
+        label.text = user.fullname
         return label
     }()
 
-    private let emailLabel: UILabel = {
+    private lazy var emailLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 14)
         label.textColor = .lightGray
-        label.text = "test@gmail.com"
+        label.text = user.email
         return label
     }()
 
 
     //MARK: - Lifecycle
 
-    override init(frame: CGRect) {
+    init(user: User, frame: CGRect) {
+        self.user = user
         super.init(frame: frame)
+
         backgroundColor = .backgroundColor
 
         addSubview(profileImageView)
@@ -57,7 +61,6 @@ class MenuHeader: UIView {
 
         addSubview(stack)
         stack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 12)
-
     }
 
     required init?(coder: NSCoder) {
